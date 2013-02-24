211d63209620e062aca8de619132f3829434af1f
Zachary Johnson <me@zachstronaut.com>
2013-02-23 23:03:19 -0600

    modified sublime text 2 command line script for opening url

--- bin/st_open_url.php
+++ bin/st_open_url.php
@@ -2,14 +2,22 @@
 <?php
 
 // { "keys": ["super+shift+o"], "command": "filter_through_command", "args": { "cmdline": "~/shortshell/bin/st_open_url.php" } }
+// Usage: cmd+l and then cmd+shift+o
 
-$line = trim(fgets(STDIN));
+$line = fgets(STDIN);
 
 preg_match_all("!https?://[^\s']+[a-z0-9]!i", $line, $matches);
 
+/*
 foreach ($matches[0] as $match) {
     system("open '" . $match . "'");
 }
+*/
 
-echo $line . "\n";
+if (!empty($matches[0])) {
+    $links = $matches[0];
+    system("open '" . $links[0] . "'");
+}
+
+echo $line;
 ?>
